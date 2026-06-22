db549be04238acfdcf10191905a746c203b2c239
Dieudonne Bahati <didopro2027@gmail.com>
2026-06-22 23:25:18 +0200

    fix: use wsgi.py

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     debug = os.environ.get("FLASK_DEBUG", "true").lower() == "true"
-    app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
+    app.run(debug=debug, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
